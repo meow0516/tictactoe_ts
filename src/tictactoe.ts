131a58b735ab2342c0f55eeb685e7644353fb706
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-22 14:03:44 +0800

    tictactoe.ts: replace by typescript style

--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -1,4 +1,4 @@
-const oxPageContainer = document.getElementById('ox_page_container');
+const oxPageContainer = document.getElementById("ox_page_container");
 const winnerArray = [
   [1, 2, 3],
   [4, 5, 6],
@@ -10,20 +10,34 @@ const winnerArray = [
   [3, 5, 7],
 ];
 
+type Player = {
+  name: string;
+  chosenNumbers: Array<number>;
+  mark: string;
+};
+
 class TicTacToe {
-  constructor(id) {
+  isOddPlayerTurn: Boolean;
+  usedNumber: number[];
+  players: { [x: string]: Player };
+  id: string;
+
+  oddPlayerName: string = "";
+  oddPlayerColor: string = "";
+
+  constructor(id: string) {
     this.isOddPlayerTurn = true;
     this.usedNumber = [];
     this.players = {
       odd: {
-        name: 'Odd',
-        chosenNumber: [],
-        mark: 'O',
+        name: "Odd",
+        chosenNumbers: [],
+        mark: "O",
       },
       even: {
-        name: 'Even',
-        chosenNumber: [],
-        mark: 'X',
+        name: "Even",
+        chosenNumbers: [],
+        mark: "X",
       },
     };
     this.id = id;
@@ -34,102 +48,111 @@ class TicTacToe {
     this.insertGameContainer(this.id);
     document
       .getElementById(this.id)
-      .querySelector('.submit')
-      .addEventListener('click', function () {
+      ?.querySelector(".submit")
+      ?.addEventListener("click", function () {
         that.submitValue(that.id);
       });
     document
       .getElementById(this.id)
-      .querySelector('.restart')
-      .addEventListener('click', function () {
+      ?.querySelector(".restart")
+      ?.addEventListener("click", function () {
         that.restartGame();
       });
   }
 
-  insertGameContainer(id) {
-    let gameContainer = document.createElement('div');
+  insertGameContainer(id: string) {
+    let gameContainer = document.createElement("div");
     gameContainer.id = id;
     gameContainer.textContent = id;
-    oxPageContainer.appendChild(gameContainer);
+    oxPageContainer?.appendChild(gameContainer);
     this.insertInput(gameContainer);
     this.insertBoxes(gameContainer);
     this.insertResetBtn(gameContainer);
   }
 
-  insertInput(upperContainer) {
-    let numberInputContainer = document.createElement('div');
-    numberInputContainer.className = 'number_input flex justify-content-center';
+  insertInput(upperContainer: HTMLElement) {
+    let numberInputContainer = document.createElement("div");
+    numberInputContainer.className = "number_input flex justify-content-center";
     upperContainer.appendChild(numberInputContainer);
 
     // input line
-    let numberInput = document.createElement('input');
-    numberInput.setAttribute('type', 'number');
-    numberInput.setAttribute('min', '1');
-    numberInput.setAttribute('max', '9');
-    numberInput.setAttribute('placeholder', 'input 1-9');
-    numberInput.id = 'inputValue';
-    numberInput.className = 'w-8rem uppercase';
+    let numberInput = document.createElement("input");
+    numberInput.setAttribute("type", "number");
+    numberInput.setAttribute("min", "1");
+    numberInput.setAttribute("max", "9");
+    numberInput.setAttribute("placeholder", "input 1-9");
+    numberInput.id = "inputValue";
+    numberInput.className = "w-8rem uppercase";
 
     // submit btn
-    let submitInput = document.createElement('input');
-    submitInput.setAttribute('type', 'submit');
-    submitInput.setAttribute('value', 'submit');
-    submitInput.className = 'submit w-5rem uppercase';
+    let submitInput = document.createElement("input");
+    submitInput.setAttribute("type", "submit");
+    submitInput.setAttribute("value", "submit");
+    submitInput.className = "submit w-5rem uppercase";
 
     // append to div
     numberInputContainer.append(numberInput);
     numberInputContainer.append(submitInput);
   }
 
-  insertBoxes(upperContainer) {
-    let oxBoxContainer = document.createElement('div');
-    oxBoxContainer.className = 'ox_box_container flex flex-wrap w-full';
+  insertBoxes(upperContainer: HTMLElement) {
+    let oxBoxContainer = document.createElement("div");
+    oxBoxContainer.className = "ox_box_container flex flex-wrap w-full";
     upperContainer.appendChild(oxBoxContainer);
     for (let i = 1; i < 10; i++) {
-      let newOxBox = document.createElement('div');
-      newOxBox.className = 'ox_box col-4';
+      let newOxBox = document.createElement("div");
+      newOxBox.className = "ox_box col-4";
 
-      let newInnerOxBox = document.createElement('div');
+      let newInnerOxBox = document.createElement("div");
       newInnerOxBox.className =
-        'inner_ox_box flex justify-content-center align-items-center h-6rem bg-blue-700 text-white text-4xl';
-      newInnerOxBox.textContent = i;
+        "inner_ox_box flex justify-content-center align-items-center h-6rem bg-blue-700 text-white text-4xl";
+      newInnerOxBox.textContent = String(i);
 
       newOxBox.appendChild(newInnerOxBox);
       oxBoxContainer.appendChild(newOxBox);
     }
   }
 
-  insertResetBtn(upperContainer) {
-    let resetContainer = document.createElement('div');
-    resetContainer.className = 'text-center';
+  insertResetBtn(upperContainer: HTMLElement) {
+    let resetContainer = document.createElement("div");
+    resetContainer.className = "text-center";
 
-    let resetBtn = document.createElement('button');
-    resetBtn.className = 'restart';
-    resetBtn.textContent = 'Restart';
+    let resetBtn = document.createElement("button");
+    resetBtn.className = "restart";
+    resetBtn.textContent = "Restart";
 
     resetContainer.appendChild(resetBtn);
     upperContainer.appendChild(resetContainer);
   }
 
-  submitValue(gameId) {
-    let inputValue = document
+  submitValue(gameId: string) {
+    let inputValueDom = document
       .getElementById(gameId)
-      .querySelector('#inputValue').valueAsNumber;
+      ?.querySelector("#inputValue");
+    let inputValue = (inputValueDom as HTMLInputElement).valueAsNumber;
 
-    let innerBox = document
-      .getElementById(gameId)
-      .querySelectorAll('.inner_ox_box');
+    const gameContainer = document.getElementById(gameId);
+
+    if (gameContainer === null) {
+      throw new Error("...");
+    }
+
+    let innerBoxes = gameContainer.querySelectorAll(".inner_ox_box");
 
     // check valid input
     let isValidInput = inputValue > 0 && inputValue < 10;
-    if (!isValidInput) return alert('please input valid number');
+    if (!isValidInput) return alert("please input valid number");
 
     // check input is used
     let isUsedInput = this.usedNumber.includes(inputValue);
-    if (isUsedInput) return alert('This number already used! Try another one.');
+    if (isUsedInput) return alert("This number already used! Try another one.");
 
     // mark box
-    this.markBox(innerBox, inputValue, this.currentPlayer);
+    this.markBox(
+      innerBoxes.item(inputValue - 1),
+      inputValue,
+      this.currentPlayer
+    );
 
     // check winner
     this.checkWinner(gameId, this.currentPlayer);
@@ -142,25 +165,35 @@ class TicTacToe {
     return this.isOddPlayerTurn ? this.players.odd : this.players.even;
   }
 
-  markBox(box, inputValue, player) {
-    box[inputValue - 1].innerHTML = player.mark;
+  markBox(box: Element, inputValue: number, player: Player) {
+    box.textContent = player.mark;
     this.usedNumber.push(inputValue);
-    player.chosenNumber.push(inputValue);
+    player.chosenNumbers.push(inputValue);
   }
 
-  checkWinner(gameId, playerInfo) {
+  checkWinner(gameId: string, playerInfo: Player) {
     for (const arr of winnerArray) {
-      if (arr.every((value) => playerInfo.chosenNumber.includes(value))) {
-        alert(gameId + ': Player ' + playerInfo.name + ' win!');
+      if (arr.every((value) => playerInfo.chosenNumbers.includes(value))) {
+        alert(gameId + ": Player " + playerInfo.name + " win!");
       }
     }
   }
   restartGame() {
-    let restartGame = confirm('Restart the game?');
+    let restartGame = confirm("Restart the game?");
     if (restartGame) {
       window.location.reload();
     }
   }
+
+  // withOddPlayerName(name: string) {
+  //   this.oddPlayerName = name;
+  //   return this;
+  // }
+
+  // withOddPlayerColor(color: string) {
+  //   this.oddPlayerColor = color;
+  //   return this;
+  // }
 }
 
 export default TicTacToe;
